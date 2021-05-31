3849340c3d73c307daf84f633fc05518668894e9
rachana <sairachanareddy1@gmail.com>
2021-05-31 19:55:02 +0530

    applications added

---
+++ HelloToast/app/src/main/java/com/example/hellotoast/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.hellotoast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    Button
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
